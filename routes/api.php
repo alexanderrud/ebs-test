019e14c1dd07cb1fbfe0a114be557374ade57ca4
arudnicenco <rudnicenko97@gmail.com>
2022-10-22 22:07:37 +0300

    small fix

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use     App\Http\Controllers\ArticleController;
+use App\Http\Controllers\ArticleController;
 use App\Http\Controllers\CategoryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
